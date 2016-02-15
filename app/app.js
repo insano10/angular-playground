8669da15cad27dbc1262393abc81b434ddf06666
insano10 <jdommett@gmail.com>
2016-02-15 12:56:33 +0000

    use directives to show, hide and repeat elements based on properties

--- app/app.js
+++ app/app.js
@@ -1,17 +1,37 @@
-(function() {
-    var app = angular.module('myApp', [ ]);
+(function ()
+{
+    var app = angular.module('myApp', []);
 
-    app.controller("UserController", function(){
+    app.controller("UserController", function ()
+    {
 
         //code in this anonymous function gets called when the controller is executed
 
-        //create a field inside the controller referencing the bob object
-        this.user = bob;
+        //create a field inside the controller referencing the users array
+        this.users = theUsers;
     });
 
-    var bob = {
-        name: "Mr Bob",
-        age: 35,
-        motto: "When the world gives you lemons, throw them at people"
-    }
+    var theUsers = [
+        {
+            name:       "Mr Bob",
+            age:        35,
+            motto:      "When the world gives you lemons, throw them at people",
+            isHungry:   false,
+            isSleeping: false
+        },
+        {
+            name:       "Mrs Jones",
+            age:        57,
+            motto:      "You can never have enough cats",
+            isHungry:   true,
+            isSleeping: false
+        },
+        {
+            name:       "Miss Kitty",
+            age:        22,
+            motto:      "If you get in my way, I will destroy you",
+            isHungry:   false,
+            isSleeping: true
+        }
+    ]
 })();
