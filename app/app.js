36798873b80a72cf475f7cdf32a3bd65b3ab025c
insano10 <jdommett@gmail.com>
2016-02-15 14:30:53 +0000

    replace usages of ng-show and ng-class checking property equality to call a named controller function instead. This is clearer and moves more logic out of the html

--- app/app.js
+++ app/app.js
@@ -21,7 +21,11 @@
 
         this.selectTab = function(setTab){
             this.tab = setTab;
-        }
+        };
+
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
     });
 
     var aBunchOfPeople = [
