f07d6a12ed6e9cd60598220e51890db960097e7e
insano10 <jdommett@gmail.com>
2016-02-15 12:23:50 +0000

    create a controller with an object field and attach it to a div to access its contents and print out values

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-angular.module('myApp', [ ])
+(function() {
+    var app = angular.module('myApp', [ ]);
+
+    app.controller("UserController", function(){
+
+        //code in this anonymous function gets called when the controller is executed
+
+        //create a field inside the controller referencing the bob object
+        this.user = bob;
+    });
+
+    var bob = {
+        name: "Mr Bob",
+        age: 35,
+        motto: "When the world gives you lemons, throw them at people"
+    }
+})();
