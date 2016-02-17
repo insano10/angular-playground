9dcf11d7ef63a3747d9edda05f69b503d5c4b131
insano10 <jdommett@gmail.com>
2016-02-17 11:48:10 +0000

    replace the hardcoded json in people.js with a call to the $http service to retrieve the data via an asynchronous call (from a static file)

--- app/js/people.js
+++ app/js/people.js
@@ -4,13 +4,22 @@
     var mod = angular.module("people", []);
 
     //create a controller inside the module 'mod'
-    mod.controller("PersonController", function ()
+    mod.controller("PersonController", ['$http', function ($http)
     {
         //code in this anonymous function gets called when the controller is executed
+        var ctrl = this;
+        ctrl.people = [];
 
         //create a field inside the controller referencing the users array
-        this.people = aBunchOfPeople;
-    });
+        $http.get('./data/people.json').then(
+            function success(response)
+            {
+                ctrl.people = response.data;
+            }, function error(e)
+            {
+                console.log("Failed to get people: " + e);
+            });
+    }]);
 
     mod.controller("FoodDiaryController", function ()
     {
@@ -60,85 +69,5 @@
             controllerAs: 'panelCtrl'
         };
     });
-
-    var aBunchOfPeople = [
-        {
-            name:     "Mr Bob",
-            age:      35,
-            motto:    "When the world gives you lemons, throw them at people",
-            isHungry: false,
-            img:      {
-                full:  "resources/images/grumpy_man.jpg",
-                thumb: "resources/images/grumpy_man_thumb.jpg"
-            },
-            diary:    [
-                {
-                    name:        "lemon",
-                    time:        10000,
-                    portionSize: 'Small',
-                    comment:     "**screws up face**",
-                    feeling:     "sad"
-                },
-                {
-                    name:        "lemon",
-                    time:        20000,
-                    portionSize: 'Small',
-                    comment:     "**screws up face**",
-                    feeling:     "sad"
-                },
-                {
-                    name:        "lemon",
-                    time:        30000,
-                    portionSize: 'Small',
-                    comment:     "**screws up face**",
-                    feeling:     "sad"
-                },
-                {
-                    name:        "lemon",
-                    time:        40000,
-                    portionSize: 'Medium',
-                    comment:     "**throws lemon at person**",
-                    feeling:     "excited"
-                }
-            ]
-        },
-        {
-            name:     "Mrs Jones",
-            age:      57,
-            motto:    "You can never have enough cats",
-            isHungry: true,
-            img:      {
-                full:  "resources/images/cat_lady.png",
-                thumb: "resources/images/cat_lady_thumb.png"
-            },
-            diary:    [
-                {
-                    name:        "carrots and hummus",
-                    time:        10000,
-                    portionSize: 'Small',
-                    comment:     "Oh no thank you, Tiddles simply hates hummus",
-                    feeling:     "sad"
-                },
-                {
-                    name:        "mackerel",
-                    time:        20000,
-                    portionSize: 'Large',
-                    comment:     "om nom nom",
-                    feeling:     "happy"
-                }
-            ]
-        },
-        {
-            name:     "Miss Kitty",
-            age:      22,
-            motto:    "If you get in my way, I will destroy you",
-            isHungry: false,
-            img:      {
-                full:  "resources/images/daria.jpg",
-                thumb: "resources/images/daria_thumb.jpg"
-            },
-            diary:    []
-        }
-    ]
 
 })();
