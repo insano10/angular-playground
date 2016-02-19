a219c1fecb972d61a081999208c3179070419ddd
insano10 <jdommett@gmail.com>
2016-02-19 16:02:46 +0000

    use angular routes and ng-view to dynamically change the content of a div depending on the url path

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 (function ()
 {
     //create a module called 'mod' that has dependencies on 'people' and 'player' modules
-    var mod = angular.module('myApp', ['people', 'player']);
+    var mod = angular.module('myApp', ['ngRoute', 'people', 'player']);
 
 })();
---
+++ app/js/routes.js
@@ -0,0 +1,14 @@
+(function(){
+
+    angular.module("myApp").config(function($routeProvider){
+
+        $routeProvider
+            .when("/users", {
+            templateUrl: "templates/pages/users/index.html"
+        })
+            .when("/highscores", {
+                templateUrl: "templates/pages/highscores/index.html"
+        })
+    });
+
+})();
